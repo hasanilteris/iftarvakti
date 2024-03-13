9ba2aa68c883da39285c8071b1065aa603d24351
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2024-03-13 03:10:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ function GetTime(){
     var minute=now.getMinutes();
     if(hour<10) hour="0"+hour;
     if(minute<10) minute="0"+minute;
-
     document.getElementById('current-time').innerText=hour+" : "+minute;
 }
 function GetCountry(){
