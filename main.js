09f57bb0c834740cde34f0b230edc1c2c683a752
Hasan İlteriş Dincer <hasanilterisdincer@gmail.com>
2024-03-13 03:10:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ function GetTime(){
     var now= new Date();
     var hour=now.getHours();
     var minute=now.getMinutes();
-
     if(hour<10) hour="0"+hour;
     if(minute<10) minute="0"+minute;
 
